fe2516ff43563bda305ec4e3cc87acac0bf29818
A-Slimani <abdullahsliman98@gmail.com>
2024-02-18 19:13:58 +1100

    updates

---
+++ car_details.py
@@ -0,0 +1,39 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from random import randint
+from datetime import date
+
+
+def get_details(driver, completed_car_list):
+  car_list = WebDriverWait(driver, randint(1, 5)).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'listing-item')))
+
+  for car in car_list:
+    current_car_obj = {}
+
+    car_state = car.find_element(By.CLASS_NAME, 'seller-location')
+    if car_state.text != 'NSW':
+      continue
+
+    car_link = car.find_element(By.TAG_NAME, 'a')
+    car_id = car_link.get_attribute('href').split('/')[-2]
+    current_car_obj.update({'ID': car_id})
+
+    current_car_obj.update({'Date': date.today().strftime("%d/%m/%Y")})
+
+    car_price = car.find_element(By.CLASS_NAME, 'price').text
+    current_car_obj.update({'Price': car_price})
+
+    car_name = car.find_element(By.TAG_NAME, 'h3').text
+    current_car_obj.update({'Year': car_name.split()[0]})
+    current_car_obj.update({'Name': ' '.join(car_name.split()[1:])})
+
+    current_car_obj.update({'State': car_state.text})
+
+    detail_list = car.find_elements(By.CLASS_NAME, 'key-details__value')
+    for detail in detail_list:
+      current_car_obj.update({detail.get_attribute('data-type'): detail.text})     
+
+    current_car_obj.update({'link': car_link.get_attribute('href')}) 
+
+    completed_car_list.append(current_car_obj)
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import WebDriverException, TimeoutException
 from datetime import date
 from random import randint
+from car_details import get_details
 import csv
 import os
 
@@ -15,65 +17,54 @@ def create_driver():
   driver = webdriver.Chrome(options=options)
   return driver
 
-
-def get_current_list(driver):
+# I think offset gives me the login error
+def get_current_list_url_offset(driver):
   offset = 0
   while True:
     try:
       url = f'https://www.carsales.com.au/cars/toyota/crown/new-south-wales-state/?sort=LastUpdated&offset={offset}'
       driver.get(url)
-
-      car_list = WebDriverWait(driver, randint(1, 5)).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'listing-item')))
-
-      for car in car_list:
-        current_car_obj = {}
-
-        car_state = car.find_element(By.CLASS_NAME, 'seller-location')
-        if car_state.text != 'NSW':
-          continue
-
-        car_price = car.find_element(By.CLASS_NAME, 'price').text
-        current_car_obj.update({'price': car_price})
-
-
-        car_name = car.find_element(By.TAG_NAME, 'h3')
-        current_car_obj.update({'year': car_name.text.split()[0]})
-        current_car_obj.update({'name': ' '.join(car_name.text.split()[1:])})
-
-        current_car_obj.update({'state': car_state.text})
-
-        detail_list = car.find_elements(By.CLASS_NAME, 'key-details__value')
-        for detail in detail_list:
-          current_car_obj.update({detail.get_attribute('data-type'): detail.text})     
-
-        car_link = car_name.find_element(By.TAG_NAME, 'a')
-        current_car_obj.update({'link': car_link.get_attribute('href')}) 
-
-        completed_car_list.append(current_car_obj)
-
+      get_details(driver, completed_car_list)
       print(f'current offset: {offset}')
       offset += 12
     except:
       print('all pages completed')
       break
 
+def get_current_list_button_offset(driver):
+  url = f"https://www.carsales.com.au/cars/toyota/crown/new-south-wales-state/?sort=LastUpdated"
+  driver.get(url)
+  while True:
+    try:
+      get_details(driver, completed_car_list)
+      next_page = WebDriverWait(driver, randint(1, 5)).until(EC.element_to_be_clickable((By.CLASS_NAME, 'page-link.next')))
+      next_page.click()
+    except: 
+      print('all pages completed')
+      break
+
 if __name__ == '__main__':
   driver = create_driver()
-  get_current_list(driver)
+  get_current_list_button_offset(driver)
   
   print(f"completed car list: {len(completed_car_list)}")
 
   if os.path.exists('cars.csv'):
     with open('cars.csv', 'r') as f:
-      write = csv.writer(f)
+      current_car_list = [row for row in csv.reader(f)]
+    
+    for car in completed_car_list:
+      if car['ID'] in current_car_list:
+        completed_car_list.remove(car)
+
+    with open('cars.csv', 'a') as f:
+      writer = csv.writer(f)
+      writer.writerows(car.values() for car in completed_car_list)
 
   else:
     with open('cars.csv', 'w') as f:
       writer = csv.writer(f)
-
-      # write the header row 
       writer.writerow(completed_car_list[0].keys())
-
-      # write the data
+      writer.writerows(car.values() for car in completed_car_list)
   
   driver.close()
